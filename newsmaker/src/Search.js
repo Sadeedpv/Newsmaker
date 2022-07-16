41d64a39ae29bb02c1192b2c50935157d865a15d
unknown <muhammedsadeed70@gmail.com>
2022-07-16 13:52:22 +0400

    Better css

--- newsmaker/src/Search.js
+++ newsmaker/src/Search.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react'
 import Footer from './components/Footer'
-import Navbar from './components/Navbar'
 import {useLocation, useNavigate} from 'react-router-dom'
 import { Spin } from '@douyinfe/semi-ui';
 import axios from 'axios';
@@ -27,18 +26,16 @@ function Search(props) {
             <>
         
                 <div style={{
-                    display: 'flex',
-                    alignItems: 'center',
-                    justifyContent: 'center',
+                    display: 'grid',
+                    placeItems:'center',
                     color: 'black',
                     fontSize: '1.6em',
                     fontWeight: '500',
                     padding: '1.4em',
-                    textAlign: 'center',
+                    textAlign:'center'
                 }}>See What's happening on {'   '}<span style={{
                     color: '#f9a825',
                     fontWeight:'650',
-                    padding:'5px',
                 }}>{input}</span></div>
                 <div ref={spinref} style={{display:'grid', placeItems:'center', marginTop:'40px'}}><Spin size='large' /></div>
                 <div className='flex-card'>
